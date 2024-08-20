de07b6de06cb17387ba73bf5b51f04c4b8d00fdd
Ashish Kushwaha <ashhome918@gmail.com>
2024-08-20 21:25:35 +0530

    microservice app using webclient

--- microservice.movie-catalog-service/src/main/java/com/ashish/microservice/movie_catalog_service/controller/MovieCatalogController.java
+++ microservice.movie-catalog-service/src/main/java/com/ashish/microservice/movie_catalog_service/controller/MovieCatalogController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,20 +21,32 @@ import java.util.stream.Collectors;
 @RequestMapping("/movieCatalog")
 public class MovieCatalogController {
 
-    @Autowired
-    private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-       Rating rating=restTemplate.getForObject("http://localhost:9191/ratingsdata/users/1/"+userId, Rating.class);
+
+       Rating rating=webClientBuilder.build().
+               get().
+               uri("http://localhost:9191/ratingsdata/users/1/"+userId).
+               retrieve()
+               .bodyToMono(Rating.class)
+               .block();
+
         System.out.println(rating);
        List<Rating> ratings=new ArrayList<>();
        ratings.add(rating);
         // get all rated movie Id's
         return ratings.stream().map((rat)->{
-            Movie movie =restTemplate.getForObject("http://localhost:9193/movieInfo/"+rat.getMovieId(), Movie.class);
+            Movie movie =webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:9193/movieInfo/"+rat.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
             return new CatalogItem().builder().
                     name(movie.getName()).ratings(rat.getRating()).
                     description("test").build();
