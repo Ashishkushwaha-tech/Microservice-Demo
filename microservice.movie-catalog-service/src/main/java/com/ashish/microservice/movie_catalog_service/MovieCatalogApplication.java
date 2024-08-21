666e2ece61aa0221dc0ca88fed76a1a4b0a78cb7
Ashish Kushwaha <ashhome918@gmail.com>
2024-08-21 12:34:03 +0530

    added circuit breaker using Hystrix

--- microservice.movie-catalog-service/src/main/java/com/ashish/microservice/movie_catalog_service/MovieCatalogApplication.java
+++ microservice.movie-catalog-service/src/main/java/com/ashish/microservice/movie_catalog_service/MovieCatalogApplication.java
@@ -1,22 +1,32 @@
 package com.ashish.microservice.movie_catalog_service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableHystrix
 public class MovieCatalogApplication {
 
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate(){
-		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory=new HttpComponentsClientHttpRequestFactory();
-		clientHttpRequestFactory.setConnectTimeout(3000);
-		return new RestTemplate(clientHttpRequestFactory);
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		factory.setConnectTimeout(5000); // 5 seconds
+		factory.setReadTimeout(5000);    // 5 seconds
+		return new RestTemplate(factory);
+	}
+	@Bean
+	public HystrixCommandAspect hystrixCommandAspect(){
+		return new HystrixCommandAspect();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogApplication.class, args);
--- microservice.movie-catalog-service/src/main/java/com/ashish/microservice/movie_catalog_service/controller/MovieCatalogController.java
+++ microservice.movie-catalog-service/src/main/java/com/ashish/microservice/movie_catalog_service/controller/MovieCatalogController.java
@@ -4,6 +4,7 @@ import com.ashish.microservice.movie_catalog_service.model.CatalogItem;
 import com.ashish.microservice.movie_catalog_service.model.Movie;
 import com.ashish.microservice.movie_catalog_service.model.Rating;
 import com.ashish.microservice.movie_catalog_service.model.UserRating;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,22 +28,26 @@ public class MovieCatalogController {
     private RestTemplate restTemplate;
 
     @GetMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-
-
-        UserRating ratings=restTemplate.getForObject("http://MICROSERVICE.RATINGS-SERVICE/ratingsdata/users/"+userId, UserRating.class);
-
-
-
-
-        return ratings.getRatingList().stream().map((rat)->{
-            // For each movie ID, call movie info service and get details
-            Movie movie =restTemplate.getForObject("http://MICROSERVICE.MOVIE/movieInfo/"+rat.getMovieId(), Movie.class);
-            // put them all together
-            return new CatalogItem().builder().
-                    name(movie.getName()).ratings(rat.getRating()).
-                    description(movie.getDescription()).build();
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        UserRating ratings= restTemplate.getForObject("http://MICROSERVICE.RATINGS-SERVICE/ratingsdata/users/" + userId, UserRating.class);
+        ;
+        return ratings.getRatingList().stream().map(rat -> {
+            Movie movie = restTemplate.getForObject("http://MICROSERVICE.MOVIE/movieInfo/" + rat.getMovieId(), Movie.class);
+            return new CatalogItem().builder()
+                    .name(movie.getName())
+                    .ratings(rat.getRating())
+                    .description(movie.getDescription())
+                    .build();
         }).collect(Collectors.toList());
-
     }
+
+    public List<CatalogItem> getFallbackCatalog(String userId) {
+        return Arrays.asList(CatalogItem.builder()
+                .name("no movie")
+                .description("nothing")
+                .ratings(0)
+                .build());
+    }
+
 }
